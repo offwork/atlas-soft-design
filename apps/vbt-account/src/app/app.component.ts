61bdc2536580b67f1cb10cd4ea31bf6553b52504
offwork <chomunist@gmail.com>
2020-08-17 16:48:47 +0300

    initial commit

---
+++ apps/vbt-account/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atlas-soft-design-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'vbt-account';
+}
